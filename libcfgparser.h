fbfdf755934af618ffc6e12a04039fcf62a1ac86
Jharrod LaFon <jlafon@lanl.gov>
2012-02-18 11:54:34 -0700

    More formatting.

--- libcfgparser.h
+++ libcfgparser.h
@@ -1,17 +1,15 @@
 #include <regex.h>
-#include "src/uthash.h"
+#include "uthash.h"
 
 #define CFGPARSER_MAX_LINE 2048
 
-struct cfgparser_section
-{
+struct cfgparser_section {
     char name[CFGPARSER_MAX_LINE];
     UT_hash_handle hh;
 };
 
-struct cfgparser_value
-{
-    struct cfgparser_section * section;
+struct cfgparser_value {
+    struct cfgparser_section* section;
     char   key[CFGPARSER_MAX_LINE];
     char value[CFGPARSER_MAX_LINE];
     UT_hash_handle hh;
@@ -19,16 +17,15 @@ struct cfgparser_value
 
 
 
-typedef struct cfgparser_context
-{
-  struct cfgparser_section * section_list;
-  struct cfgparser_value * value_list;
-  regex_t comment_pattern;
-  regex_t section_pattern;
-  regex_t value_pattern;
-  regex_t key_pattern;
+typedef struct cfgparser_context {
+    struct cfgparser_section* section_list;
+    struct cfgparser_value* value_list;
+    regex_t comment_pattern;
+    regex_t section_pattern;
+    regex_t value_pattern;
+    regex_t key_pattern;
 } cfgparser_context_t;
 
 /* Read in a cfg file */
-int cfgparser_load_file( char * file, cfgparser_context_t * context );
-int cfgparser_get_str( char * section, char * key, char * value, cfgparser_context_t * context ); 
+int cfgparser_load_file(char* file, cfgparser_context_t* context);
+int cfgparser_get_str(char* section, char* key, char* value, cfgparser_context_t* context);
